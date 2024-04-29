73c71983d50ff6960460c81cca65453d3d8a36b0
watwatos <alichamas.22@hotmail.com>
2024-04-29 18:43:11 +0300

    frontend: added-background

--- client/src/common/components/Layout.tsx
+++ client/src/common/components/Layout.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="bg">{children}</div>;
+  return (
+    <div className="bg min-h-screen bg-cover bg-center bg-no-repeat">
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
