ad2073c0fb207a1df2b52333b93836410f245de7
watwatos <alichamas.22@hotmail.com>
2024-04-29 20:13:58 +0300

    frontend: added-links-to-nav

--- client/src/common/components/MobileMenu.tsx
+++ client/src/common/components/MobileMenu.tsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
-const MobileMenu = () => {
+import { motion } from "framer-motion";
+
+const MobileMenu = ({ links, navigate }: any) => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="text-4xl">
-      <RxHamburgerMenu />
-      <div className="flex flex-col "></div>
+    <div>
+      <button
+        type="button"
+        className="text-4xl "
+        onClick={() => setOpen((o) => !o)}
+      >
+        <RxHamburgerMenu />
+      </button>
+      {open && (
+        <motion.div
+          initial={{ y: 20 }}
+          animate={{ y: 0, transition: { delay: 0.2, stiffness: 20 } }}
+          className="flex flex-col bg-primary p-14 absolute top-18 right-2 text-md rounded-md items-center gap-5"
+        >
+          <div className="flex flex-col gap-5">
+            {links.map((link: any, i: number) => (
+              <button
+                key={i}
+                className="opacity-80 hover:opacity-100 transition-all duration-300"
+                onClick={() => navigate(link.destination)}
+              >
+                {link.name}
+              </button>
+            ))}
+          </div>
+
+          <button className="btn-primary-white">Login</button>
+        </motion.div>
+      )}
     </div>
   );
 };
--- client/src/common/components/Navbar.tsx
+++ client/src/common/components/Navbar.tsx
@@ -1,15 +1,38 @@
 import MobileMenu from "./MobileMenu";
-
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const links = [
+    { name: "About", destination: "/about" },
+    { name: "Contact", destination: "/contact" },
+    { name: "Docs", destination: "/docs" },
+  ];
   return (
-    <div className="py-6 px-12 flex justify-between items-center">
-      <div>
-        <img src="/logo.png" alt="logo" className="w-[90px] h-[60px] " />
+    <div className="py-6 px-12 flex justify-between items-center relative">
+      <div className="flex gap-5 items-center">
+        <img
+          src="/logo.png"
+          alt="logo"
+          className="w-[80px] h-[60px] lg:w-[100px] lg:h-[80px] "
+        />
+        <div className="flex gap-3 items-center">
+          {links.map((link: any, i: number) => (
+            <button
+              key={i}
+              className="text-lg opacity-80 hover:opacity-100 transition-all duration-300"
+              onClick={() => navigate(link.destination)}
+            >
+              {link.name}
+            </button>
+          ))}
+        </div>
       </div>
 
       <div className="md:hidden">
-        <MobileMenu />
+        <MobileMenu links={links} navigate={navigate} />
       </div>
+      <button className="btn-primary-white hidden md:block ">Login</button>
     </div>
   );
 };
