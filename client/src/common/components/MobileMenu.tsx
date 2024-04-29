963299d55e7967e084f35568f9c850479c2d3ae6
watwatos <alichamas.22@hotmail.com>
2024-04-29 19:10:29 +0300

    frontend: added-mobile-menu

---
+++ client/src/common/components/MobileMenu.tsx
@@ -0,0 +1,10 @@
+import { RxHamburgerMenu } from "react-icons/rx";
+const MobileMenu = () => {
+  return (
+    <div className="text-4xl">
+      <RxHamburgerMenu />
+    </div>
+  );
+};
+
+export default MobileMenu;
--- client/src/common/components/Navbar.tsx
+++ client/src/common/components/Navbar.tsx
@@ -1,5 +1,17 @@
+import MobileMenu from "./MobileMenu";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="py-6 px-12 flex justify-between items-center">
+      <div>
+        <img src="/logo.png" alt="logo" className="w-[90px] h-[60px] " />
+      </div>
+
+      <div className="md:hidden">
+        <MobileMenu />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
