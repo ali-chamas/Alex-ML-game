1131691abbe46e53be6d7b3973de5bc52436ba78
watwatos <alichamas.22@hotmail.com>
2024-04-29 18:24:52 +0300

    frontend: added-auth-routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing-page";
+import Auth from "./pages/auth-page";
 
 const App = () => {
   <BrowserRouter>
     <Routes>
       <Route element={<Landing />} path="/" />
+      <Route element={<Auth />} path="/auth" />
     </Routes>
   </BrowserRouter>;
 };
