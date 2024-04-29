49a3752ee73a3a69ec1898f74bf07ec4eb6f0b42
watwatos <alichamas.22@hotmail.com>
2024-04-29 18:29:39 +0300

    frontend: fixed-landing-error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,16 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing-page";
 import Auth from "./pages/auth-page";
 
 const App = () => {
-  <BrowserRouter>
-    <Routes>
-      <Route element={<Landing />} path="/" />
-      <Route element={<Auth />} path="/auth" />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Landing />} path="/" />
+        <Route element={<Auth />} path="/auth" />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
