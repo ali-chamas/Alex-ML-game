b47bb62b98a62fc97ca98e52609024c734f1d220
watwatos <alichamas.22@hotmail.com>
2024-04-29 18:43:57 +0300

    frontend: added-navbar-to-layout

--- client/src/common/components/Layout.tsx
+++ client/src/common/components/Layout.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from "react";
+import Navbar from "./Navbar";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="bg min-h-screen bg-cover bg-center bg-no-repeat">
+      <Navbar />
       {children}
     </div>
   );
