991bafdc2dcb6663a5ea18c852286e4f3711d25a
watwatos <alichamas.22@hotmail.com>
2024-04-30 17:44:21 +0300

    frontend: updated-auth-routes

--- client/src/tools/protected-routes/LoggedInRoutes.tsx
+++ client/src/tools/protected-routes/LoggedInRoutes.tsx
@@ -3,8 +3,18 @@ import { UserContext, UserContextType } from "../../context/userContext";
 import { Navigate, Outlet } from "react-router-dom";
 
 const LoggedInProtection = () => {
-  const { token } = useContext(UserContext) as UserContextType;
-  return token ? <Navigate to={"/profile"} /> : <Outlet />;
+  const { token, user } = useContext(UserContext) as UserContextType;
+  return token ? (
+    user?.role == "user" ? (
+      <Navigate to={"/profile"} />
+    ) : user?.role == "creator" ? (
+      <Navigate to={"/creator"} />
+    ) : (
+      <Navigate to={"admin"} />
+    )
+  ) : (
+    <Outlet />
+  );
 };
 
 export default LoggedInProtection;
