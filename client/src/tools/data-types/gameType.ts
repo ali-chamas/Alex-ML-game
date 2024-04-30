f9041f7be5acd711511615883edac31e66465dc6
watwatos <alichamas.22@hotmail.com>
2024-04-30 20:37:26 +0300

    frontend: created-game-type

---
+++ client/src/tools/data-types/gameType.ts
@@ -0,0 +1,13 @@
+export interface gameType {
+  name: string;
+  description: string;
+  image: string;
+  solution: string;
+  hint: string;
+  level: string;
+  order: number;
+  type: string;
+  isApproved: boolean;
+  iscomplete: boolean;
+  mmodel: any;
+}
