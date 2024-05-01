5b15dece49656ada03dd475f4d29721ad33df032
watwatos <alichamas.22@hotmail.com>
2024-05-01 21:07:43 +0300

    frontend: added-activeGame-functionality

--- client/src/pages/user/single-game-page/index.tsx
+++ client/src/pages/user/single-game-page/index.tsx
@@ -1,8 +1,24 @@
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { GamesContext, GamesContextType } from "../../../context/gamesContext";
+import { gameType } from "../../../tools/data-types/gameType";
 
 const SingleGame = () => {
   const { gameId } = useParams();
-  console.log(gameId);
+
+  const { globalGames } = useContext(GamesContext) as GamesContextType;
+
+  const [activeGame, setActiveGame] = useState<gameType | any>();
+
+  const getActiveGame = () => {
+    setActiveGame(globalGames.filter((game) => game._id !== gameId));
+  };
+
+  console.log(activeGame);
+
+  useEffect(() => {
+    getActiveGame();
+  }, []);
 
   return <div>index</div>;
 };
