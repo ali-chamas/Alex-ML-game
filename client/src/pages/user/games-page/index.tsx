ecd1a75221a46f492482dc1117bcf221a5cbd6ef
watwatos <alichamas.22@hotmail.com>
2024-05-01 11:39:34 +0300

    frontend: added-cards-swiper

--- client/src/pages/user/games-page/index.tsx
+++ client/src/pages/user/games-page/index.tsx
@@ -6,6 +6,8 @@ import { sendRequest } from "../../../tools/request-method/request";
 import Gamecard from "./components/Gamecard";
 import Loader from "../../../common/components/Loader";
 import { UserContext, UserContextType } from "../../../context/userContext";
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Games = () => {
   const { user } = useContext(UserContext) as UserContextType;
@@ -48,7 +50,7 @@ const Games = () => {
   console.log(filteredGames);
 
   return (
-    <div className="flex flex-col mt-12 gap-12 min-h-[80vh]">
+    <div className="flex flex-col mt-12 gap-12 min-h-[80vh] ">
       <h1 className="text-primary text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-[48px] self-center">
         CHOOSE YOUR MISSION
       </h1>
@@ -60,15 +62,41 @@ const Games = () => {
           <Loader />
         </div>
       ) : (
-        <div className="self-center">
+        <Swiper
+          spaceBetween={30}
+          breakpoints={{
+            576: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1200: {
+              slidesPerView: 3,
+            },
+            1900: {
+              slidesPerView: 4,
+            },
+          }}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          className="w-full cursor-grab "
+        >
           {filteredGames.length > 0 ? (
             filteredGames.map((game, i) => (
-              <Gamecard key={i} game={game} user={user} />
+              <div>
+                <SwiperSlide>
+                  <Gamecard key={i} game={game} user={user} />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Gamecard key={i} game={game} user={user} />
+                </SwiperSlide>
+              </div>
             ))
           ) : (
-            <p>No games yet</p>
+            <SwiperSlide>No games yet</SwiperSlide>
           )}
-        </div>
+        </Swiper>
       )}
     </div>
   );
