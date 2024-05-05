8499e6b19fcc2f8b21ad26c3e802deaa21245303
watwatos <alichamas.22@hotmail.com>
2024-05-05 09:47:38 +0300

    frontend: updated-hint

--- client/src/pages/user/single-game-page/components/HintPopup.tsx
+++ client/src/pages/user/single-game-page/components/HintPopup.tsx
@@ -1,33 +1,25 @@
 import React from "react";
 import { IoMdClose } from "react-icons/io";
 import alex from "../../../../assets/logo.png";
-const HintPopup = ({
-  hint,
-  setOpen,
-}: {
-  hint: string;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}) => {
+import { FaLightbulb } from "react-icons/fa6";
+import {
+  Popover,
+  PopoverContent,
+  PopoverHandler,
+} from "@material-tailwind/react";
+const HintPopup = ({ hint }: string | undefined) => {
   return (
     <>
-      <div className="flex justify-between items-center w-full">
-        <h1 className=" text-lg ">Here's a hint!</h1>
-
-        <button onClick={() => setOpen(false)} className="text-xl ">
-          <IoMdClose />
-        </button>
-      </div>
-      <div className="flex  gap-5  items-center justify-start h-full relative">
-        <img
-          src={alex}
-          className="w-[100px] lg:w-[150px]"
-          alt=""
-          loading="lazy"
-        />
-        <h1 className="lg:text-base bg-black/25 p-3 rounded-lg text-sm">
-          {hint}
-        </h1>
-      </div>
+      <Popover placement="bottom">
+        <PopoverHandler>
+          <button className="rounded-full  bg-primary border p-2 hover:opacity-80">
+            <FaLightbulb />
+          </button>
+        </PopoverHandler>
+        <PopoverContent className="bg-primary flex flex-col items-center gap-2">
+          {hint && <p>{hint}</p>}
+        </PopoverContent>
+      </Popover>
     </>
   );
 };
--- client/src/pages/user/single-game-page/components/TrainOption.tsx
+++ client/src/pages/user/single-game-page/components/TrainOption.tsx
@@ -1,5 +1,5 @@
 import { IoSettings } from "react-icons/io5";
-import { FaLightbulb } from "react-icons/fa";
+
 import {
   Input,
   Popover,
@@ -9,21 +9,21 @@ import {
 import { useState } from "react";
 import { sendRequest } from "../../../../tools/request-method/request";
 import { gameType } from "../../../../tools/data-types/gameType";
-import PopupLayout from "../../../../common/components/PopupLayout";
+
 import HintPopup from "./HintPopup";
 
 const TrainOption = ({
   game,
   setTrigger,
 }: {
-  game: gameType | any;
+  game: gameType | undefined;
   setTrigger: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   const [label, setLabel] = useState<string>("");
   const [openHint, setOpenHint] = useState<boolean>(false);
 
   const addLabel = async () => {
-    const gameBody = { gameId: game._id, label: label };
+    const gameBody = { gameId: game?._id, label: label };
     try {
       const res = await sendRequest("POST", "/user/add_label", gameBody);
       setTrigger((t) => !t);
@@ -39,12 +39,7 @@ const TrainOption = ({
         <IoSettings />
       </button>
       <div className="flex gap-2 lg:gap-4 items-center">
-        <button
-          className="rounded-full  bg-primary border p-2 hover:opacity-80"
-          onClick={() => setOpenHint(true)}
-        >
-          <FaLightbulb />
-        </button>
+        <HintPopup hint={game?.hint} />
         <Popover placement="bottom">
           <PopoverHandler>
             <button className="btn-primary-dark">add label</button>
@@ -63,12 +58,6 @@ const TrainOption = ({
           </PopoverContent>
         </Popover>
       </div>
-      {openHint && (
-        <PopupLayout
-          children={<HintPopup setOpen={setOpenHint} hint={game.hint} />}
-          open={openHint}
-        />
-      )}
     </div>
   );
 };
