07575a74260ca3b4576a5137933e85f57c7466ec
watwatos <alichamas.22@hotmail.com>
2024-05-08 20:59:57 +0300

    frontend: added-light-mode-game-card

--- client/src/pages/user/games-page/components/Gamecard.tsx
+++ client/src/pages/user/games-page/components/Gamecard.tsx
@@ -50,7 +50,7 @@ const Gamecard = ({
   };
 
   return (
-    <div className="flex flex-col items-center gap-2 rounded-md bg-[#031C28] bg-opacity-75 h-[320px] w-[300px]">
+    <div className="flex flex-col items-center gap-2 rounded-md bg-primary  bg-opacity-75 h-[320px] w-[300px] border-2 border-black/10">
       <div className="relative">
         <img src={`${apiUrl}/${game.image}`} alt="" className="rounded-t-md" />
         <button
@@ -62,7 +62,9 @@ const Gamecard = ({
       </div>
       <div className="px-5  flex flex-col gap-3 items-center">
         <h1 className="font-bold">{game.name}</h1>
-        <small className="text-white/70 ">{game.description}</small>
+        <small className="text-black/70 dark:text-white/70 ">
+          {game.description}
+        </small>
         {game.isComplete ? (
           <button
             className="btn-primary-white w-[200px]"
