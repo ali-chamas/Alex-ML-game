ad3ef8838f5f544bc74290c7f517249dc3018cff
watwatos <alichamas.22@hotmail.com>
2024-05-10 09:59:34 +0300

    frontend: fetched-users-in layout

--- client/src/pages/creator/home/components/CounterCards.tsx
+++ client/src/pages/creator/home/components/CounterCards.tsx
@@ -3,21 +3,36 @@ import {
   GamesContext,
   GamesContextType,
 } from "../../../../context/gamesContext";
+import { useSelector } from "react-redux";
+import { userType } from "../../../../tools/data-types/userType";
 
 const CounterCards = () => {
   const { globalGames } = useContext(GamesContext) as GamesContextType;
 
+  const usersState = useSelector((state: any) => state.users.users);
+
   const [gamesCount, setGamesCount] = useState<number>(0);
   const [usersCount, setUsersCount] = useState<number>(0);
+
+  const getCreators = () => {
+    const creators = usersState.filter(
+      (user: userType) => user.role == "creator"
+    );
+    setUsersCount(creators.length);
+  };
 
   useEffect(() => {
     setGamesCount(globalGames.length);
   }, [globalGames.length]);
 
+  useEffect(() => {
+    getCreators();
+  }, [usersState.length]);
+
   return (
     <div className="flex gap-5 md:gap-10 lg:gap-20">
       <div className="px-10 py-6 bg-primary rounded-lg border lg:text-xl">
-        10 creators
+        {usersCount} creators
       </div>
       <div className="px-10 py-6 bg-primary rounded-lg border lg:text-xl">
         {gamesCount} games
--- client/src/redux/users/index.ts
+++ client/src/redux/users/index.ts
@@ -13,14 +13,14 @@ const userSlice = createSlice({
   name: "users",
   initialState,
   reducers: {
-    setUsers: (state, action: PayloadAction<UserState>) => {
-      state.users = action.payload.users;
+    setUsers: (state, action: PayloadAction<userType[]>) => {
+      state.users = action.payload;
     },
   },
 });
 
 export const { setUsers } = userSlice.actions;
 
-export const selectUser = (state: { users: UserState }) => state.users.users;
+export const selectUser = (state: { users: UserState }) => state.users;
 
 export default userSlice.reducer;
