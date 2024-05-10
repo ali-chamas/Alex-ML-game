4da390faa08a60d678c7c0a4a24b1cc604e81d12
watwatos <alichamas.22@hotmail.com>
2024-05-10 23:21:12 +0300

    frontend: added-edit-game-functionality

--- client/src/pages/creator/games/components/EditGamePopup.tsx
+++ client/src/pages/creator/games/components/EditGamePopup.tsx
@@ -1,9 +1,17 @@
 import { FileUploader } from "react-drag-drop-files";
 import { gameType } from "../../../../tools/data-types/gameType";
 import GamePopups from "./GamePopups";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { sendRequest } from "../../../../tools/request-method/request";
+import {
+  GamesContext,
+  GamesContextType,
+} from "../../../../context/gamesContext";
+import toast, { Toaster } from "react-hot-toast";
 
 const EditGamePopup = ({ game }: { game: gameType }) => {
+  const { setCreatorTrigger } = useContext(GamesContext) as GamesContextType;
+
   const infoArray = [
     { title: "Title", value: game?.name, reqTitle: "name" },
     { title: "Hint", value: game?.hint, reqTitle: "hint" },
@@ -12,10 +20,25 @@ const EditGamePopup = ({ game }: { game: gameType }) => {
     { title: "Level", value: game?.level, reqTitle: "level" },
   ];
 
-  const editGame = async () => {};
+  const editGame = async (file: any, reqTitle: string) => {
+    const formData = new FormData();
+    formData.append(reqTitle, file);
+    try {
+      const res = await sendRequest(
+        "PUT",
+        `/creator/update_game/${game._id}`,
+        formData
+      );
+      setCreatorTrigger((t) => !t);
+      toast.success("updated");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
+      <Toaster />
       {infoArray.map((info, i) => (
         <GamePopups
           gameId={game._id}
@@ -29,12 +52,12 @@ const EditGamePopup = ({ game }: { game: gameType }) => {
         <FileUploader
           name="file"
           label="Image"
-          handleChange={(file: any) => editGame(file)}
+          handleChange={(file: any) => editGame(file, "image")}
         />
         <FileUploader
           name="file"
           label="Solution"
-          handleChange={(file: any) => editGame(file)}
+          handleChange={(file: any) => editGame(file, "solution")}
         />
       </div>
     </>
