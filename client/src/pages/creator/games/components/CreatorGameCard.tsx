a348fb1b40319a9811a8e6806b9599474a5f21d8
watwatos <alichamas.22@hotmail.com>
2024-05-10 17:21:30 +0300

    frontend: added-games-creator-popups

--- client/src/pages/creator/games/components/CreatorGameCard.tsx
+++ client/src/pages/creator/games/components/CreatorGameCard.tsx
@@ -1,29 +1,52 @@
+import { useState } from "react";
 import { apiUrl } from "../../../../tools/api-url/apiUrl";
 import { gameType } from "../../../../tools/data-types/gameType";
+import PopupLayout from "../../../../common/components/PopupLayout";
 
 const CreatorGameCard = ({ game }: { game: gameType }) => {
+  const [openEdit, setOpenEdit] = useState<any | gameType>(false);
+
   return (
-    <div className="flex flex-col items-center gap-2 rounded-md bg-primary  bg-opacity-75 h-[320px] w-[300px] border-2 border-black/10">
-      <div className="relative">
-        <img src={`${apiUrl}/${game.image}`} alt="" className="rounded-t-md" />
-        {game.isApproved ? (
-          <p className="absolute top-0 m-3 right-0 bg-green-500/60 rounded-full text-white p-3 ">
-            Yes
-          </p>
-        ) : (
-          <p className="absolute top-0 m-3 right-0 bg-red-500/60 rounded-full text-white px-4 p-3 ">
-            No
-          </p>
-        )}
+    <>
+      <div className="flex flex-col items-center gap-2 rounded-md bg-primary  bg-opacity-75 h-[320px] w-[300px] border-2 border-black/10">
+        <div className="relative">
+          <img
+            src={`${apiUrl}/${game.image}`}
+            alt=""
+            className="rounded-t-md"
+          />
+          {game.isApproved ? (
+            <p className="absolute top-0 m-3 right-0 bg-green-500/60 rounded-full text-white p-3 ">
+              Yes
+            </p>
+          ) : (
+            <p className="absolute top-0 m-3 right-0 bg-red-500/60 rounded-full text-white px-4 p-3 ">
+              No
+            </p>
+          )}
+        </div>
+        <div className="px-5  flex flex-col gap-3 items-center">
+          <h1 className="font-bold">{game.name}</h1>
+          <small className="text-black/70 dark:text-white/70 ">
+            {game.description}
+          </small>
+          <button
+            className="btn-primary-white"
+            onClick={() => setOpenEdit(game)}
+          >
+            Edit
+          </button>
+        </div>
       </div>
-      <div className="px-5  flex flex-col gap-3 items-center">
-        <h1 className="font-bold">{game.name}</h1>
-        <small className="text-black/70 dark:text-white/70 ">
-          {game.description}
-        </small>
-        <button className="btn-primary-white">Edit</button>
-      </div>
-    </div>
+      {openEdit && (
+        <PopupLayout
+          children={""}
+          open={openEdit}
+          setOpen={setOpenEdit}
+          title="Edit Game"
+        />
+      )}
+    </>
   );
 };
 
--- client/src/pages/creator/games/index.tsx
+++ client/src/pages/creator/games/index.tsx
@@ -1,21 +1,39 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GamesContext, GamesContextType } from "../../../context/gamesContext";
-import Gamecard from "../../user/games-page/components/Gamecard";
+
 import CreatorGameCard from "./components/CreatorGameCard";
+
+import PopupLayout from "../../../common/components/PopupLayout";
 
 const CreatorGames = () => {
   const { globalGames } = useContext(GamesContext) as GamesContextType;
+
+  const [openAdd, setOpenAdd] = useState<boolean>(false);
+
   return (
     <div className="flex flex-col gap-10 my-10 md:m-10 w-full">
       <div className="flex justify-between">
         <h1 className="text-primary text-3xl">Our Games</h1>
-        <button className="btn-primary-dark">+Game</button>
+        <button
+          className="btn-primary-dark"
+          onClick={() => setOpenAdd((open) => !open)}
+        >
+          + Game
+        </button>
       </div>
       <div className="flex flex-wrap gap-5">
         {globalGames.map((game, i) => (
           <CreatorGameCard game={game} key={i} />
         ))}
       </div>
+      {openAdd && (
+        <PopupLayout
+          children={""}
+          open={openAdd}
+          setOpen={setOpenAdd}
+          title="Add A Game"
+        />
+      )}
     </div>
   );
 };
