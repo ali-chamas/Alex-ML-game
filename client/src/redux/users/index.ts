5e3430dd411a6b688a3e1fcaaf95f8c3d52f959e
watwatos <alichamas.22@hotmail.com>
2024-05-10 09:18:01 +0300

    frontend: added-users-initialState

--- client/src/redux/users/index.ts
+++ client/src/redux/users/index.ts
@@ -1,2 +1,10 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { userType } from "../../tools/data-types/userType";
+
+interface UserState {
+  users: userType[] | [];
+}
+
+const initialState: UserState = {
+  users: [],
+};
