24621287c921eb5d0b5a4ea374591b14f75d82a0
watwatos <alichamas.22@hotmail.com>
2024-05-11 12:06:20 +0300

    frontend: added-table-layout

--- client/src/pages/admin/home/index.tsx
+++ client/src/pages/admin/home/index.tsx
@@ -1,11 +1,11 @@
 import CounterCards from "../../creator/home/components/CounterCards";
+import UsersTable from "../../creator/home/components/UsersTable";
 
 const AdminHome = () => {
   return (
     <div className="flex flex-col gap-10 m-5 md:m-10 w-full items-center">
-      <div className="flex gap-3">
-        <CounterCards />
-      </div>
+      <CounterCards />
+      <UsersTable />
     </div>
   );
 };
