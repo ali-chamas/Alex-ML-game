13d07742ff1869df8d26f28f1faca7e1a6715e9c
watwatos <alichamas.22@hotmail.com>
2024-05-11 13:16:32 +0300

    frontend: added-home-inputs-admin

--- client/src/pages/creator/home/components/UsersTable.tsx
+++ client/src/pages/creator/home/components/UsersTable.tsx
@@ -1,9 +1,20 @@
-import { Card, Typography } from "@material-tailwind/react";
+import {
+  Card,
+  Typography,
+  Input,
+  Select,
+  Option,
+} from "@material-tailwind/react";
 import { useSelector } from "react-redux";
 import { userType } from "../../../../tools/data-types/userType";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import {
+  DarkModeContext,
+  DarkModeContextType,
+} from "../../../../context/DarkModeContext";
 const UsersTable = () => {
   const users = useSelector((state: any) => state.users.users);
+  const { isDarkMode } = useContext(DarkModeContext) as DarkModeContextType;
 
   const [filteredUsers, setFilteredUsers] = useState<userType[] | []>(users);
 
@@ -23,100 +34,119 @@ const UsersTable = () => {
   const TABLE_ROWS = filteredUsers;
 
   return (
-    <Card className="h-[500px] overflow-auto w-full bg-white/0">
-      <table className="w-full min-w-max table-auto text-left">
-        <thead className="sticky top-0 ">
-          <tr>
-            {TABLE_HEAD.map((head) => (
-              <th
-                key={head}
-                className="border-b border-blue-gray-100 dark:border-black bg-cyan-50  dark:bg-black/70 p-4 w-[10%]"
-              >
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="font-normal leading-none opacity-70"
+    <div className="flex flex-col w-full gap-5">
+      <div className="flex justify-between items-center">
+        <h1 className="md:text-xl xl:text-2xl">{users.length} Users</h1>
+        <div>
+          <Input
+            label="Search"
+            type="search"
+            color={isDarkMode ? "white" : "black"}
+          />
+        </div>
+        <div>
+          <Select label="Level">
+            <Option value="beginner">Beginner</Option>
+            <Option value="intermediate">Intermediate</Option>
+            <Option value="advanced">Advanced</Option>
+          </Select>
+        </div>
+      </div>
+      <Card className="h-[450px] overflow-auto w-full bg-white/0">
+        <table className="w-full min-w-max table-auto text-left">
+          <thead className="sticky top-0 ">
+            <tr>
+              {TABLE_HEAD.map((head) => (
+                <th
+                  key={head}
+                  className="border-b border-blue-gray-100 dark:border-black bg-cyan-50  dark:bg-black/70 p-4 w-[10%]"
                 >
-                  {head}
-                </Typography>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {TABLE_ROWS.map((user: userType, index: number) => {
-            const isLast = index === TABLE_ROWS.length - 1;
-            const classes = isLast
-              ? "p-4"
-              : "p-4 border-b border-blue-gray-50 dark:border-black";
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal leading-none opacity-70"
+                  >
+                    {head}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {TABLE_ROWS.map((user: userType, index: number) => {
+              const isLast = index === TABLE_ROWS.length - 1;
+              const classes = isLast
+                ? "p-4"
+                : "p-4 border-b border-blue-gray-50 dark:border-black";
 
-            return (
-              <tr
-                key={user._id}
-                className="even:bg-cyan-50 dark:even:bg-black/20 dark:odd:bg-[#031C28]"
-              >
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {user.firstName}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {user.lastName}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {user.username}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {user.age}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {user.role}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    as="a"
-                    href="#"
-                    variant="small"
-                    color="blue-gray"
-                    className="btn-primary-danger text-sm w-[90px]"
-                  >
-                    Delete
-                  </Typography>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </Card>
+              return (
+                <tr
+                  key={user._id}
+                  className="even:bg-cyan-50 odd:bg-white/40 dark:even:bg-black/20 dark:odd:bg-[#031C28]"
+                >
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {user.firstName}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {user.lastName}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {user.username}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {user.age}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {user.role}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      as="a"
+                      href="#"
+                      variant="small"
+                      color="blue-gray"
+                      className="btn-primary-danger text-sm w-[90px]"
+                    >
+                      Delete
+                    </Typography>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </Card>
+    </div>
   );
 };
 
