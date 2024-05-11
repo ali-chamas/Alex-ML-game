5885151590b0c9d9d8954a9d86e79a24cfd5cc5c
watwatos <alichamas.22@hotmail.com>
2024-05-11 17:49:59 +0300

    frontend: added-games-admin

---
+++ client/src/pages/admin/games/components/AdminGameCard.tsx
@@ -0,0 +1,34 @@
+import { apiUrl } from "../../../../tools/api-url/apiUrl";
+import { gameType } from "../../../../tools/data-types/gameType";
+
+const AdminGameCard = ({ game }: { game: gameType }) => {
+  return (
+    <div className="flex flex-col items-center gap-2 rounded-md bg-primary  bg-opacity-75 h-[320px] w-[300px] border-2 border-black/10 ">
+      <div className="relative">
+        <img
+          src={`${apiUrl}/${game.image}`}
+          alt=""
+          className="rounded-t-md h-[150px] w-[300px]"
+        />
+        {game.isApproved ? (
+          <button className="absolute top-0 m-3 right-0 bg-green-500/60 rounded-full text-white p-3 ">
+            Yes
+          </button>
+        ) : (
+          <button className="absolute top-0 m-3 right-0 bg-red-500/60 rounded-full text-white px-4 p-3 ">
+            No
+          </button>
+        )}
+      </div>
+      <div className="px-5  flex flex-col gap-3 items-center">
+        <h1 className="font-bold">{game.name}</h1>
+        <small className="text-black/70 dark:text-white/70 ">
+          {game.description}
+        </small>
+        <button className="btn-primary-white">Edit</button>
+      </div>
+    </div>
+  );
+};
+
+export default AdminGameCard;
--- client/src/pages/admin/games/index.tsx
+++ client/src/pages/admin/games/index.tsx
@@ -1,5 +1,20 @@
+import { useContext } from "react";
+import { GamesContext, GamesContextType } from "../../../context/gamesContext";
+import AdminGameCard from "./components/AdminGameCard";
+
 const AdminGames = () => {
-  return <div>AdminGames</div>;
+  const { globalGames } = useContext(GamesContext) as GamesContextType;
+
+  return (
+    <div className="flex flex-col p-10">
+      <h1 className="text-primary text-lg m:text-xl lg:text-2xl">Games</h1>
+      <div className="flex flex-wrap justify-center md:justify-start gap-4">
+        {globalGames.map((game, i) => (
+          <AdminGameCard game={game} key={i} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AdminGames;
