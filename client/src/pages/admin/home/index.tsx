c39b042467fb09253f915e5df9e1cd02f4e92b6d
watwatos <alichamas.22@hotmail.com>
2024-05-11 11:19:48 +0300

    frontend: added-count-cards-to-admin

--- client/src/pages/admin/home/index.tsx
+++ client/src/pages/admin/home/index.tsx
@@ -1,5 +1,13 @@
+import CounterCards from "../../creator/home/components/CounterCards";
+
 const AdminHome = () => {
-  return <div>index</div>;
+  return (
+    <div className="flex flex-col gap-10 m-5 md:m-10 w-full items-center">
+      <div className="flex gap-3">
+        <CounterCards />
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
