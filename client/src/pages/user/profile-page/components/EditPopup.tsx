ff8da9e8691af8750726b5f2992e137fc36dc6cb
watwatos <alichamas.22@hotmail.com>
2024-05-14 16:56:51 +0300

    frontend: added-inputs-with-save-btn

--- client/src/pages/user/profile-page/components/EditPopup.tsx
+++ client/src/pages/user/profile-page/components/EditPopup.tsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { userType } from "../../../../tools/data-types/userType";
 import InputPopups from "./InputPopups";
+import { sendRequest } from "../../../../tools/request-method/request";
+import { UserContext, UserContextType } from "../../../../context/userContext";
+import toast, { Toaster } from "react-hot-toast";
 
 const EditPopup = ({ user }: { user: userType | null | undefined }) => {
+  const { setUserTrigger } = useContext(UserContext) as UserContextType;
+
   const [userInfo, setUserInfo] = useState<{} | userType>({});
 
   const infoArray = [
@@ -12,8 +17,36 @@ const EditPopup = ({ user }: { user: userType | null | undefined }) => {
     { title: "age", value: user?.age, reqTitle: "age" },
   ];
 
+  interface errorType {
+    status: number;
+  }
+
+  const updateUser = async () => {
+    try {
+      const res = await sendRequest("PUT", "/user/update_my_info", userInfo);
+
+      setUserTrigger((t) => !t);
+      toast.success(`user updated succesfully`, {
+        className: "dark:bg-blue-gray-900 dark:text-white",
+      });
+    } catch (error: errorType | any) {
+      console.log(error);
+
+      if (error?.response.status == 400)
+        toast.error("username already taken", {
+          className: "dark:text-white dark:bg-blue-gray-900",
+        });
+      else {
+        toast.error("something went wrong", {
+          className: "dark:bg-blue-gray-900",
+        });
+      }
+    }
+  };
+
   return (
-    <>
+    <div className="flex flex-col items-center gap-6">
+      <Toaster />
       {infoArray.map((info, i) => (
         <InputPopups
           reqTitle={info.reqTitle}
@@ -21,9 +54,13 @@ const EditPopup = ({ user }: { user: userType | null | undefined }) => {
           value={info.value}
           key={i}
           setUserInfo={setUserInfo}
+          userInfo={userInfo}
         />
       ))}
-    </>
+      <button className="btn-primary-white" onClick={updateUser}>
+        Save
+      </button>
+    </div>
   );
 };
 
--- client/src/pages/user/profile-page/components/InputPopups.tsx
+++ client/src/pages/user/profile-page/components/InputPopups.tsx
@@ -1,72 +1,38 @@
-import {
-  Input,
-  Popover,
-  PopoverContent,
-  PopoverHandler,
-} from "@material-tailwind/react";
-import { useContext, useState } from "react";
-import { sendRequest } from "../../../../tools/request-method/request";
-import { UserContext, UserContextType } from "../../../../context/userContext";
-import toast, { Toaster } from "react-hot-toast";
+import { Input } from "@material-tailwind/react";
+import { useContext } from "react";
+
 import {
   DarkModeContext,
   DarkModeContextType,
 } from "../../../../context/DarkModeContext";
+import { userType } from "../../../../tools/data-types/userType";
 
 const InputPopups = ({
   title,
   value,
   reqTitle,
+  setUserInfo,
+  userInfo,
 }: {
   title: string | undefined;
   value: string | number | undefined;
   reqTitle: string | undefined;
+  setUserInfo: any;
+  userInfo: userType | any;
 }) => {
-  interface errorType {
-    status: number;
-  }
-
-  const [input, setInput] = useState<string | number>(value);
-  const { setUserTrigger } = useContext(UserContext) as UserContextType;
   const { isDarkMode } = useContext(DarkModeContext) as DarkModeContextType;
-
-  const updateUser = async () => {
-    const reqBody = {
-      [reqTitle as string]: input,
-    };
-    try {
-      const res = await sendRequest("PUT", "/user/update_my_info", reqBody);
-
-      setUserTrigger((t) => !t);
-      toast.success(`${title} updated succesfully`, {
-        className: "dark:bg-blue-gray-900 dark:text-white",
-      });
-    } catch (error: errorType | any) {
-      console.log(error);
-
-      if (error?.response.status == 400)
-        toast.error("username already taken", {
-          className: "dark:text-white dark:bg-blue-gray-900",
-        });
-      else {
-        toast.error("something went wrong", {
-          className: "dark:bg-blue-gray-900",
-        });
-      }
-    }
-  };
 
   return (
     <div className="flex gap-2 w-full items-center justify-center mt-2">
-      <Toaster />
-
       <Input
         label={title}
         type={typeof value == "number" ? "number" : "text"}
         className="w-full"
         color={isDarkMode ? "white" : "black"}
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+        defaultValue={value}
+        onChange={(e) =>
+          setUserInfo({ ...userInfo, [reqTitle as string]: e.target.value })
+        }
       />
     </div>
   );
