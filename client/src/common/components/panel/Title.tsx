a10e8b066484576262b7f89f1d998fd6c17f84f2
watwatos <alichamas.22@hotmail.com>
2024-05-15 10:41:54 +0300

    frontend: fixed-title-width

--- client/src/common/components/panel/Title.tsx
+++ client/src/common/components/panel/Title.tsx
@@ -1,6 +1,6 @@
 const Title = ({ title }: { title: string }) => {
   return (
-    <div className="flex justify-between sticky top-0 bg-primary p-5 rounded-lg z-10">
+    <div className="flex justify-between sticky top-0 bg-primary p-5 rounded-lg z-10 w-full">
       <h1 className="text-primary text-lg lg:text-3xl">{title}</h1>
     </div>
   );
