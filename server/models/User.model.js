429fb7886dc548129f21e415067621090cfb128f
watwatos <alichamas.22@hotmail.com>
2024-05-18 23:31:55 +0300

    backend: fixed-unique-order-error

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -55,7 +55,7 @@ const userSchema = new mongoose.Schema(
         _id: { type: mongoose.Schema.Types.ObjectId, ref: "Game", index: true },
         finished: { type: Boolean, default: false },
         model: { type: modelSchema, default: Model },
-        order: { type: Number, unique: true },
+        order: { type: Number },
       },
     ],
   },
